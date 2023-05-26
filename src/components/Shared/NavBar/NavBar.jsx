f5600e1be1ed5d0c134fb989cb0b002eef15eb27
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-26 23:31:45 +0600

    starter

---
+++ src/components/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            
+        </div>
+    );
+};
+
+export default NavBar;
