3f2d71adc03a110401ce9c070910c8d3923f14f7
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-27 00:10:45 +0600

    navbar

--- src/components/Shared/NavBar/NavBar.jsx
+++ src/components/Shared/NavBar/NavBar.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+
+import Container from '../Container';
+import Logo from './Logo';
 
 const NavBar = () => {
     return (
         <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'> 
+            <Container>
+            <div className="flex flex-row items-center justify-between md:gap-0 gap-3 ">
+                <div><Logo/></div>
+                <div>Search</div>
+                <div>Menu</div>
+            </div>
+            </Container>
+            </div>
             
         </div>
     );
